0ae6541b9ae48e613be646f2c0b3f41b5b09ada4
Kollan House <kollan.house@ctr.com>
2016-07-03 23:43:31 +0000

    added filtering for finding the lowest prices.

--- index.php
+++ index.php
@@ -9,6 +9,8 @@ $function = 'Booking.aspx?';
 $date = '07%2F08%2F2016';
 $query = 'Company_ID=4&Return=0&Departure_Location_ID=18&Departure_Date='.$date.'&Arrival_Location_ID=3&Return_Date=&Weight_ID%5B9%5D=0&Weight_ID%5B10%5D=1&Weight_ID%5B7%5D=0&Weight_ID%5B8%5D=0&Action=Booking&Sorting=0';
 
+$minPrice = 10000;
+
 $url = $baseURL . $function . $query;
 
 $dom = new Dom;
@@ -16,15 +18,30 @@ $dom->load($url); // '<div class="all"><p>Hey bro, <a href="google.com">click he
 
 $a = $dom->find('.ui-tablerow-global-schedules');
 foreach($a as $flight) {
-	$flightNumber = $flight->find('.ui-tablecolumn-global-schedules-schedule');
-	$flightNumber = preg_replace('|/Twin Otter|', '', $flightNumber);
-	$flightNumber = preg_replace('|Flight #|', '', $flightNumber);
-	echo '<strong>'.$flightNumber .'</strong>';
-
-	$time = $flight->find('.ui-tablecolumn-global-schedules-departure-time');
 	$fare = $flight->find('.ui-link-global-schedules-dialog-fare');
-	echo $time;
-	echo $fare;
-	echo '<br>';
+	$fare = $fare->find('span');
+	$fare = $fare->innerHtml;
+	$fare = str_replace('$', '', $fare); // /&.*?;/
+	foreach ($fare as $fare) {
+		if($fare < $minPrice) {
+			$minPrice = $fare;
+		} else {
+			echo 'price broken';
+		}
+	}
+	$fare = floatval($fare);
+	if ($fare <= $minPrice) {
+		$minPrice = $fare;
+		$flightNumber = $flight->find('.ui-tablecolumn-global-schedules-schedule');
+		$flightNumber = preg_replace('|/Twin Otter|', '', $flightNumber);
+		$flightNumber = preg_replace('|Flight #|', '', $flightNumber);
+		$time = $flight->find('.ui-tablecolumn-global-schedules-departure-time');
+		echo $time;
+		echo $fare;
+		echo '<br>';
+	} else {
+		echo 'NOT LOWEST<br>';
+	}
+	
 }
 exit;
